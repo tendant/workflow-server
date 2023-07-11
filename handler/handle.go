670162136bd9d4ff0e04794637f74d874757fb50
Yangye Zhu <yangye.zhu@bryzi.io>
2023-07-11 11:58:51 -0700

    add handler

--- handler/handle.go
+++ handler/handle.go
@@ -10,6 +10,26 @@ type Handle struct {
 	Slog *slog.Logger
 }
 
+type ExpenseInput struct {
+	ExpenseId int `in:"path=eid"`
+}
+
 func (h Handle) Hello(w http.ResponseWriter, r *http.Request) {
+	render.PlainText(w, r, http.StatusText(http.StatusOK))
+}
+
+func (h Handle) ListExpenses(w http.ResponseWriter, r *http.Request) {
+	render.PlainText(w, r, http.StatusText(http.StatusOK))
+}
+
+func (h Handle) GetExpense(w http.ResponseWriter, r *http.Request) {
+	render.PlainText(w, r, http.StatusText(http.StatusOK))
+}
+
+func (h Handle) ApproveExpense(w http.ResponseWriter, r *http.Request) {
+	render.PlainText(w, r, http.StatusText(http.StatusOK))
+}
+
+func (h Handle) DeclineExpense(w http.ResponseWriter, r *http.Request) {
 	render.PlainText(w, r, http.StatusText(http.StatusOK))
 }
--- handler/route.go
+++ handler/route.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"github.com/ggicci/httpin"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -8,7 +9,10 @@ func Routes(r *chi.Mux, handle Handle) {
 	r.Group(func(r chi.Router) {
 		r.Route("/api/v2", func(r chi.Router) {
 			r.Get("/", handle.Hello)
-			r.Get("/expenses", handle.Hello)
+			r.Get("/expenses", handle.ListExpenses)
+			r.With(httpin.NewInput(ExpenseInput{})).Get("/expenses/{eid}", handle.GetExpense)
+			r.With(httpin.NewInput(ExpenseInput{})).Post("/expenses/{eid}/approve", handle.ApproveExpense)
+			r.With(httpin.NewInput(ExpenseInput{})).Post("/expenses/{eid}/decline", handle.DeclineExpense)
 		})
 	})
 }
