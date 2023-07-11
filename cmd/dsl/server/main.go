f4f855fbe0790962fa65429e20c72e28c4852cf7
Yangye Zhu <yangye.zhu@bryzi.io>
2023-07-11 14:12:39 -0700

    cmd dsl server: add static

--- cmd/dsl/server/main.go
+++ cmd/dsl/server/main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"embed"
 	"github.com/tendant/workflow-server/app"
 	"github.com/tendant/workflow-server/handler"
+	"go.temporal.io/sdk/client"
+	"os"
 )
+
+//go:embed static
+var ef embed.FS
 
 func main() {
 	app := app.Default()
+	slog := app.Slog
+
+	// Create the client object just once per process
+	c, err := client.Dial(client.Options{})
+	if err != nil {
+		slog.Error("unable to create Temporal client", "err", err)
+		os.Exit(1)
+	}
+	defer c.Close()
 
 	handle := handler.Handle{
-		Slog: app.Slog,
+		Slog:   slog,
+		Client: c,
+		Ef:     ef,
 	}
 
 	handler.Routes(app.R, handle)
